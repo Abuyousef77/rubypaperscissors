d51c730e57fb041e85efbc7f41f11e911fa58cdd
Benjamin Bressler, Daniel Garcia and Dawson Mortenson <daniel@>
2017-09-06 15:28:56 -0700

    refactor the gameloop into the acutal game script. This eliminates unecessary code from the class definition

--- lib/plays.rb
+++ lib/plays.rb
@@ -16,45 +16,17 @@ class Game
     @p2_score = score
   end
 
-  def gameLoop
-    continue = true
-    while continue
-      puts "Pick a move P1: 'ruby', 'paper', 'scissors'"
-      player1 = gets.chomp
-      puts "Pick a move P2: 'ruby', 'paper', 'scissors'"
-      player2 = gets.chomp
-      result = wins?(player1,player2)
-      if result === "tie"
-        puts "It was a tie, shoot again"
-      elsif result === "invalid"
-        # Play again
-      else
-        winner(result)
-        puts "Player 1 Score = #{self.p1_score}"
-        puts "Player 2 Score = #{self.p2_score}"
-        puts "Want to play again? y/n"
-        decision = gets.chomp
-        if decision === "n"
-          continue = false
-        end
-      end
-    end
-  end
-
   def wins?(play1, play2)
-    if {"scissors" => "test", "paper" => "test", "ruby" => "test"}.has_key?(play2)
+    key_check = {"scissors" => "test", "paper" => "test", "ruby" => "test"}
+    if key_check.has_key?(play2) & key_check.has_key?(play1)
       if play1 === "ruby"
         {"scissors" => true, "paper" => false, "ruby" => "tie"}.fetch(play2)
       elsif play1 === "paper"
         {"scissors" => false, "paper" => "tie", "ruby" => true}.fetch(play2)
       elsif play1 === "scissors"
-        {"scissors" => "tie", "paper" => true, "ruby" => false, }.fetch(play2)
-      else
-        puts "Some invalid move"
-        "invalid"
+        {"scissors" => "tie", "paper" => true, "ruby" => false}.fetch(play2)
       end
     else
-      puts "Some invalid move"
       "invalid"
     end
   end
--- the_game.rb
+++ the_game.rb
@@ -3,15 +3,25 @@ require './lib/plays.rb'
 
 # Creates a game object
 purple_monkey = Game.new()
-purple_monkey.gameLoop()
-
-# blue_banana = Game.new()
-# blue_banana.gameLoop()
-
-# puts "Final Scores for Purple Monkey Game:"
-# puts "#{purple_monkey.p1_score}"
-# puts "#{purple_monkey.p2_score}"
-
-# puts "Final Scores for Blue Banana Game:"
-# puts "#{blue_banana.p1_score}"
-# puts "#{blue_banana.p2_score}"
+continue = true
+while continue
+  puts "Pick a move P1: 'ruby', 'paper', 'scissors'"
+  player1 = gets.chomp
+  puts "Pick a move P2: 'ruby', 'paper', 'scissors'"
+  player2 = gets.chomp
+  result = purple_monkey.wins?(player1, player2)
+  if result === "tie"
+    puts "It's a tie, shoot again"
+  elsif result === "invalid"
+    puts "Some invalid move"
+  else
+    purple_monkey.winner(result)
+    puts "Player 1 Score = #{purple_monkey.p1_score}"
+    puts "Player 2 Score = #{purple_monkey.p2_score}"
+    puts "Want to play again? y/n"
+    decision = gets.chomp
+    if decision === "n"
+      continue = false
+    end
+  end
+end
